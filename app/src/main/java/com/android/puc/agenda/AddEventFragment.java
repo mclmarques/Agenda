b8708b373931ef0813b59f8a01436c20c2b364a6
ViniciusHVAR <vinicius.martins@hvarconsulting.com.br>
2024-10-29 22:12:21 -0300

    feat: buttons functionalities on progress.

--- app/src/main/java/com/android/puc/agenda/AddEventFragment.java
+++ app/src/main/java/com/android/puc/agenda/AddEventFragment.java
@@ -1,4 +1,44 @@
 package com.android.puc.agenda;
 
-public class AddEventFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class AddEventFragment extends Fragment {
+
+    private TextView selectedDateTextView;
+    private TextView tvSelectedTime;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.add_event, container, false);
+
+        selectedDateTextView = view.findViewById(R.id.selectedDateTextView);
+        tvSelectedTime = view.findViewById(R.id.tvSelectedTime);
+
+        Button btnData = view.findViewById(R.id.btnData);
+        btnData.setOnClickListener(v -> openDatePicker());
+
+        Button btnHora = view.findViewById(R.id.btnHora);
+        btnHora.setOnClickListener(v -> {
+            TimePickerFragment timePickerFragment = new TimePickerFragment();
+            timePickerFragment.setOnTimeSetListener(time -> tvSelectedTime.setText(time));
+            timePickerFragment.show(getParentFragmentManager(), "timePicker");
+        });
+
+        return view;
+    }
+
+    private void openDatePicker() {
+        DatePickerFragment datePickerFragment = new DatePickerFragment();
+        datePickerFragment.setOnDateSetListener(date -> selectedDateTextView.setText(date));
+        datePickerFragment.show(getParentFragmentManager(), "datePicker");
+    }
 }
--- app/src/main/java/com/android/puc/agenda/EventListFragment.java
+++ app/src/main/java/com/android/puc/agenda/EventListFragment.java
@@ -1,4 +1,63 @@
 package com.android.puc.agenda;
 
-public class EventListFragment {
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.android.puc.agenda.data.Event;
+import java.util.List;
+
+public class EventListFragment extends Fragment {
+
+    private TextView tvEvents;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.event_list, container, false);
+
+        tvEvents = view.findViewById(R.id.tvEvents);
+        Button btnPickDate = view.findViewById(R.id.btnPickDate);
+
+        btnPickDate.setOnClickListener(v -> openDatePicker());
+
+        return view;
+    }
+
+    private void openDatePicker() {
+        DatePickerFragment datePickerFragment = new DatePickerFragment();
+
+        datePickerFragment.setOnDateSetListener(this::fetchEventsForDate);
+
+
+        datePickerFragment.show(getParentFragmentManager(), "datePicker");
+    }
+
+    private void fetchEventsForDate(String date) {
+        ((MainActivity) requireActivity()).getAllEvents(events -> displayEvents(events, date));
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void displayEvents(List<Event> events, String selectedDate) {
+        StringBuilder eventsText = new StringBuilder();
+
+        for (Event event : events) {
+            if (event.getDate().toString().equals(selectedDate)) {
+                eventsText.append(event.getTitle()).append(": ").append(event.getDescription()).append("\n");
+            }
+        }
+
+        if (eventsText.length() == 0) {
+            tvEvents.setText("Nenhum evento encontrado para a data " + selectedDate);
+        } else {
+            tvEvents.setText(eventsText.toString());
+        }
+    }
 }
